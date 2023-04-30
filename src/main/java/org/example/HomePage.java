f5589e4ab2e35944c5b269705d8881f0893039d7
Jatinpatel <shivam1819patel@gmail.com>
2023-04-30 15:41:53 +0100

    first commit

--- src/main/java/org/example/HomePage.java
+++ src/main/java/org/example/HomePage.java
@@ -3,7 +3,8 @@ package org.example;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-public class HomePage extends Utils{
+public class HomePage extends Utils
+{
     private By _registraionButton = By.className("ico-register");
 
 public void clickonregistrationButton()
